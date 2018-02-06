675f77bfd882067e3974edd741d2ac7b258086ec
dtormey <dtormey@usc.edu>
2018-02-06 14:28:22 -0800

    changed server addr

--- ee250-lab04/part2/udpClient.py
+++ ee250-lab04/part2/udpClient.py
@@ -6,7 +6,7 @@ def Main():
     host = '127.0.0.1'
     port = 1023
 
-    server_addr = '127.0.0.1'
+    server_addr = '192.168.1.225'
 
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
     s.bind((host,port))
